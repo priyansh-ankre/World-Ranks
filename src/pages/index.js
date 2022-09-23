660ad1028f0c216fb2b6ea5c547ca755d66728bd
Priyansh Ankre <priyansh.ankre@magicedtech.com>
2022-09-24 00:04:16 +0530

    added search bar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,26 @@
-import Layout from "../components/Layout";
+import Layout from '../components/Layout/Layout'
+import SearchInput from "../components/SearchInput/SearchInput";
+import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export default function Home ({countries}) {
+  console.log(countries);
   return (
-    <Layout />
+    <Layout className={styles.counts}>
+      <div>Found {countries.length} countries</div>
+      <SearchInput placeholder = 'Filter by Name, Region or SubRegion'  />
+    </Layout>
   )
+}
+
+export const getStaticProps = async () => {
+  const response = await fetch(
+    'https://restcountries.com/v2/all');
+
+// Parse the JSON
+  const data = await response.json();
+  return {
+    props: {
+      countries : data
+    }
+  }
 }
