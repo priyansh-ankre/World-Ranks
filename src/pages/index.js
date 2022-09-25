4dcfc4adbbb0bf56b96e06ba289261ef9815612a
Priyansh Ankre <priyansh.ankre@magicedtech.com>
2022-09-25 19:42:07 +0530

    Added search functionality countriesTable

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,31 @@
+import { useState } from 'react';
 import CountriesTable from '../components/CountriesTable/CountriesTable';
 import Layout from '../components/Layout/Layout'
 import SearchInput from "../components/SearchInput/SearchInput";
 import styles from "../styles/Home.module.css";
 
 export default function Home ({countries}) {
-  console.log(countries);
+  
+  const [keyword, setKeyword] = useState("");
+
+  const filteredCountries = countries.filter(
+    (country) =>
+      country.name.toLowerCase().includes(keyword) ||
+      country.region.toLowerCase().includes(keyword) ||
+      country.subregion.toLowerCase().includes(keyword)
+  );
+
+  const onInputChange = (e) => {
+    e.preventDefault();
+
+    setKeyword(e.target.value.toLowerCase().trim());
+  };
+  
   return (
     <Layout className={styles.counts}>
       <div>Found {countries.length} countries</div>
-      <SearchInput placeholder = 'Filter by Name, Region or SubRegion'  />
-      <CountriesTable countries={countries} />
+      <SearchInput placeholder = 'Filter by Name, Region or SubRegion' onChange={onInputChange} />
+      <CountriesTable countries={filteredCountries} />
     </Layout>
   )
 }
